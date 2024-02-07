93cee835fd5690dcd8115decf7afe41df5f65967
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-07 13:42:12 +0500

    done:  Helpers, Colors

---
+++ lib/config/colors.dart
@@ -0,0 +1,5 @@
+import 'package:todo_app/config/imorts.dart';
+
+class AppColor {
+  static Color primary = HexColor.fromHex("#154FA6");
+}
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,3 +1,4 @@
+import 'package:todo_app/config/colors.dart';
 import 'package:todo_app/config/imorts.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,8 +12,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Text("data")],
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        child: Column(children: [
+          Container(
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              color: AppColor.primary,
+            ),
+            child: Text(
+              "Label",
+              style:
+                  TextStyle(color: AppColor.white, fontWeight: FontWeight.w600),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ]),
       ),
     );
   }
