edffa6fe19a2236213380e4cf1bc6dc986ad1168
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-17 16:08:12 +0500

    CircleContainer

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -7,6 +7,6 @@ class AppColor {
   static Color blue9 = HexColor.fromHex("#05425C");
   static Color blue7 = HexColor.fromHex("#154FA6");
   static Color blue5 = HexColor.fromHex("#3F9FFF");
-  static Color blue3 = const Color.fromARGB(180, 199, 215, 234);
+  static Color blue3 = Color.fromARGB(126, 194, 220, 252);
   static Color grey7 = Color.fromARGB(180, 117, 115, 115);
 }
--- lib/pages/CreateTask.dart
+++ lib/pages/CreateTask.dart
@@ -10,44 +10,99 @@ class CreateTask extends StatefulWidget {
 class _CreateTaskState extends State<CreateTask> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Row(
+        const Positioned(top: -100, child: CircleContainer()),
+        const Positioned(top: -30, left: -60, child: CircleContainer()),
+        Column(
           children: [
-            Spacer(),
-            Text("Create a Task",
-                style: TextStyle(
-                    color: AppColor.blue5,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18)),
-            Spacer(),
-            SvgPicture.asset(AppIcon.search, color: AppColor.blue5),
-          ],
-        ),
-        //task  title
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 30),
-          child: TextField(
-            style: TextStyle(
-                color: AppColor.blue7,
-                fontWeight: FontWeight.w800,
-                fontSize: 20),
-            cursorColor: AppColor.blue5,
-            decoration: InputDecoration(
-                label: Text("Task title",
+            Row(
+              children: [
+                const Spacer(),
+                Text("Create a Task",
                     style: TextStyle(
                         color: AppColor.blue5,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400)),
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: AppColor.blue, width: 1.0)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: AppColor.blue, width: 2.0))),
-          ),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18)),
+                const Spacer(),
+                SvgPicture.asset(AppIcon.search, color: AppColor.blue5),
+              ],
+            ),
+            //task  title
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+              child: TextField(
+                style: TextStyle(
+                    color: AppColor.blue7,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20),
+                cursorColor: AppColor.blue5,
+                decoration: InputDecoration(
+                    label: Text("Task title",
+                        style: TextStyle(
+                            color: AppColor.blue5,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blue, width: 1.0)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColor.blue, width: 2.0))),
+              ),
+            ),
+            const SizedBox(height: 50),
+            //task discription
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [AppColor.blue5, AppColor.blue7])),
+              child: Column(
+                children: [
+                  TextField(
+                    style: TextStyle(
+                        color: AppColor.blue7,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20),
+                    cursorColor: AppColor.blue5,
+                    decoration: InputDecoration(
+                        label: Text("Task title",
+                            style: TextStyle(
+                                color: AppColor.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: AppColor.blue, width: 1.0)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: AppColor.blue, width: 2.0))),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
-
-        //task discription
       ],
+    );
+  }
+}
+
+class CircleContainer extends StatelessWidget {
+  const CircleContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+          color: AppColor.blue3, borderRadius: BorderRadius.circular(100)),
     );
   }
 }
